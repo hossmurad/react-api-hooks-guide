110598cc6a5dd912f3867ccbe8138dfd63ad7126
hossMurad <hoss.murad@gmail.com>
2025-03-29 01:52:53 +0600

    useEffect calling api

--- src/UseEffect.jsx
+++ src/UseEffect.jsx
@@ -1,18 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const UseEffect = () => {
-    const [counter, setCounter] = useState(0);
+    const [data, setData] = useState(null);
 
     useEffect(() => {
-        console.log(`Hello Bangladesh ${counter}`);
-    }, [counter]);
+        callApi();
+    }, []);
 
-    return (
-        <div>
-            <p>Counter: {counter}</p>
-            <button onClick={() => setCounter(counter + 1)}>UseEffect Example (Check Console)</button>
-        </div>
-    );
+    const callApi = async () => {
+        try {
+            let response = await fetch("https://ecommerce-api.codesilicon.com/api/ListProductByCategory/1");
+            let responseData = await response.json();
+            setData(responseData['data']);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    if (data === null) {
+        return (
+            <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        );
+    } else {
+        return (
+            <div className="container">
+                <div className="row">
+                    {data.map((item, i) => (
+                        <div className="col-md-3" key={i}>
+                            <div className="card p-2">
+                                <img className="card-img-top" src={item.image} alt="Product" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{item.title}</h5>
+                                    <h4 className="card-text">Price: ${item.price}</h4><br/>
+                                    <button className="btn btn-primary" >Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
 };
 
 export default UseEffect;
